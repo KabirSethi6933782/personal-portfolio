95a5ad77ff9c470970d2df2fb405f8d1aa913228
Kabir Sethi <ks19pt@brocku.ca>
2025-05-24 21:44:46 -0400

    navBar code fixed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
   useEffect(() => {
     if (open) document.body.style.overflow = "hidden";
     else document.body.style.overflow = "";
-    return () => (document.body.style.overflow = "");
+    return () => {document.body.style.overflow = ""};
   }, [open]);
 
   return (
